fca8fb0890d39845e8f1794a569c7416a24622de
quguiyou <quguiyou1076@qq.com>
2024-05-26 11:27:56 +0800

    8.6

---
+++ src/WorkerThread/A8_6/Main.java
@@ -0,0 +1,24 @@
+package WorkerThread.A8_6;
+
+public class Main {
+    public static void main(String[] args) {
+        Channel channel = new Channel(5); // 个人线程的个数
+        channel.startWorkers();
+        ClientThread alice = new ClientThread("Alice", channel);
+        ClientThread bobby = new ClientThread("Bobby", channel);
+        ClientThread chris = new ClientThread("Chris", channel);
+        alice.start();
+        bobby.start();
+        chris.start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+        }
+
+        alice.stopThread();
+        bobby.stopThread();
+        chris.stopThread();
+        channel.stopAllWorkers();
+    }
+}
